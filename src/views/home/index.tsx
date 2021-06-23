b415b0f19d02112f6bff89661ea079439c571f8b
tjumma <tim@jumma.pro>
2021-06-23 04:31:21 +0300

    31

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -44,10 +44,10 @@ const joinedBufferToBuffer = function (joinedBuffer: string) {
 }
 
 const unityContext = new UnityContext({
-  loaderUrl: "unity_build/29_board_4_no_capture.loader.js",
-  dataUrl: "unity_build/29_board_4_no_capture.data",
-  frameworkUrl: "unity_build/29_board_4_no_capture.framework.js",
-  codeUrl: "unity_build/29_board_4_no_capture.wasm",
+  loaderUrl: "unity_build/31.loader.js",
+  dataUrl: "unity_build/31.data",
+  frameworkUrl: "unity_build/31.framework.js",
+  codeUrl: "unity_build/31.wasm",
   streamingAssetsUrl: "StreamingAssets"
 });
 
