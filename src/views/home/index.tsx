4f6cdfdb446574eca4b238ae3f626a5f4f8370a8
tjumma <tim@jumma.pro>
2021-07-31 02:21:46 +0300

    summoner_4

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -55,10 +55,10 @@ var lastMessageNonce = 0;
 var oldCardIndex = 0
 
 const unityContext = new UnityContext({
-  loaderUrl: "unity_build/summoner_3_stringify.loader.js",
-  dataUrl: "unity_build/summoner_3_stringify.data",
-  frameworkUrl: "unity_build/summoner_3_stringify.framework.js",
-  codeUrl: "unity_build/summoner_3_stringify.wasm",
+  loaderUrl: "unity_build/summoner_4.loader.js",
+  dataUrl: "unity_build/summoner_4.data",
+  frameworkUrl: "unity_build/summoner_4.framework.js",
+  codeUrl: "unity_build/summoner_4.wasm",
   streamingAssetsUrl: "StreamingAssets"
 });
 
